11adb03bde0c59bb83bebab726774a18624230bf
Sylvain Julé <sylvain.jule@free.fr>
2018-10-01 23:17:35 +0200

    Composer support

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . DS . 'lib' . DS . 'functions.php';
+
+Kirby::plugin('sylvainjule/colorextractor', [
+	'options' => array(
+		'average' => false,
+		'fallbackColor' => '#ffffff',
+	),
+    'fields' => require_once __DIR__ . DS . 'lib' . DS . 'fields.php',
+    'hooks'  => require_once __DIR__ . DS . 'lib' . DS . 'hooks.php',
+    'routes' => require_once __DIR__ . DS . 'lib' . DS . 'routes.php',
+]);
--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php
 
-require_once __DIR__ . DS . 'lib' . DS . 'functions.php';
-
-Kirby::plugin('sylvainjule/colorextractor', [
-	'options' => array(
-		'average' => false,
-		'fallbackColor' => '#ffffff',
-	),
-    'fields' => require_once __DIR__ . DS . 'lib' . DS . 'fields.php',
-    'hooks'  => require_once __DIR__ . DS . 'lib' . DS . 'hooks.php',
-    'routes' => require_once __DIR__ . DS . 'lib' . DS . 'routes.php',
-]);
+require_once __DIR__ . '/vendor/autoload.php';
