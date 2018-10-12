dd1a548e6bbdabcf7cf88c63ee364f15d3a1c2a8
Sylvain Julé <sylvain.jule@free.fr>
2018-10-12 19:07:43 +0200

    Make sure thumb url is prefixed with http_host

--- lib/functions.php
+++ lib/functions.php
@@ -6,7 +6,9 @@ use League\ColorExtractor\Palette;
 
 function extractColor($image, $size, $fallbackColor) {
 	$thumb     = $image->resize($size);
-	$palette   = Palette::fromFilename($thumb->url(), Color::fromHexToInt($fallbackColor));
+	$url       = $thumb->url();
+	$url       = substr($url, 0, 4) === 'http' ? $url : absoluteThumbUrl($url);
+	$palette   = Palette::fromFilename($url, Color::fromHexToInt($fallbackColor));
 	$extractor = new ColorExtractor($palette);
 	$colors    = $extractor->extract(1);
 	$hex       = Color::fromIntToHex($colors[0]);
@@ -15,4 +17,14 @@ function extractColor($image, $size, $fallbackColor) {
 	$image->update(array(
 		'color' => $hex
 	));
+}
+
+function absoluteThumbUrl() {
+    if (isset($_SERVER['HTTPS'])){
+        $protocol = ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != "off") ? "https" : "http";
+    }
+    else {
+        $protocol = 'http';
+    }
+    return $protocol . "://" . $_SERVER['HTTP_HOST'] . $url;
 }
