bdb487d839128e4b1f9e16449bb889ba769380e5
Sylvain Julé <sylvain.jule@free.fr>
2018-10-16 22:59:30 +0200

    Got rid of DS

--- config.php
+++ config.php
@@ -1,13 +1,15 @@
 <?php
 
-require_once __DIR__ . '/lib/colorextractor.php';
+if (!class_exists('SylvainJule\ColorExtractor')) {
+	require_once __DIR__ . '/lib/colorextractor.php';
+}
 
 Kirby::plugin('sylvainjule/colorextractor', [
 	'options' => array(
 		'average' => false,
 		'fallbackColor' => '#ffffff',
 	),
-    'fields' => require_once __DIR__ . DS . 'lib' . DS . 'fields.php',
-    'hooks'  => require_once __DIR__ . DS . 'lib' . DS . 'hooks.php',
-    'routes' => require_once __DIR__ . DS . 'lib' . DS . 'routes.php',
+    'fields' => require_once __DIR__ . '/lib/fields.php',
+    'hooks'  => require_once __DIR__ . '/lib/hooks.php',
+    'routes' => require_once __DIR__ . '/lib/routes.php',
 ]);
--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php
 
-if (!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
-
 require_once __DIR__ . '/vendor/autoload.php';
