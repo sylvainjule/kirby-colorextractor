369551b3a3b373b3fbe742a089a1b43101df40fe
Sylvain Julé <sylvain.jule@free.fr>
2019-03-17 17:31:51 +0100

    Follow the new Pluginkit guidelines

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+if (!class_exists('SylvainJule\ColorExtractor')) {
+	require_once __DIR__ . '/lib/colorextractor.php';
+}
+
+Kirby::plugin('sylvainjule/colorextractor', [
+	'options' => array(
+		'cache' => true,
+		'average' => false,
+		'fallbackColor' => '#ffffff',
+	),
+    'fields' => require_once __DIR__ . '/lib/fields.php',
+    'hooks'  => require_once __DIR__ . '/lib/hooks.php',
+    'translations' => array(
+        'en' => require_once __DIR__ . '/lib/languages/en.php',
+        'de' => require_once __DIR__ . '/lib/languages/de.php',
+        'fr' => require_once __DIR__ . '/lib/languages/fr.php',
+    ),
+    'api' => array(
+    	'routes' => require_once __DIR__ . '/lib/routes.php',
+    ),
+]);
